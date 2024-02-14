ab1aee47cb34588f3af9e308dcc459cbfe49e2ca
Napat662110186 <Napat_srichaisarakul@cmu.ac.th>
2024-02-14 18:04:16 +0700

    Ex2

--- app/src/main/java/org/dii/oop/App.java
+++ app/src/main/java/org/dii/oop/App.java
@@ -1,5 +1,5 @@
 package org.dii.oop;
-import org.dii.oop.lesson06.exercise01.Lesson;
+import org.dii.oop.lesson06.exercise02.Lesson;
 public class App {
     public static void main(String[] args) {
         Lesson.run();
